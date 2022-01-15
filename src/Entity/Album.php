d19920380262988f6fbbb9cb83459a8cc0e25bd9
Santi <sdiaz@webimpacto.es>
2022-01-15 10:04:18 +0100

    Se crea el controlador principal de los albums

---
+++ src/Entity/Album.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlbumRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlbumRepository::class)]
+class Album
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 2000, nullable: true)]
+    private $description;
+
+    #[ORM\ManyToOne(targetEntity: Socialnet::class, inversedBy: 'albums')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $socialnet;
+
+    #[ORM\Column(type: 'string', length: 12, nullable: true)]
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSocialnet(): ?Socialnet
+    {
+        return $this->socialnet;
+    }
+
+    public function setSocialnet(?Socialnet $socialnet): self
+    {
+        $this->socialnet = $socialnet;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(?string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
